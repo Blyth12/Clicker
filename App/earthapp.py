126163df7832a475ef16e2017d67ff1fd58698c9
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2021-10-08 13:29:09 +0100

    Update earthapp.py

--- App/earthapp.py
+++ App/earthapp.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 from tkinter import *
 
 
@@ -16,12 +17,16 @@ f2 = Frame(root)
 for frame in (f1, f2):
     frame.grid(row=5, column=5, sticky='news')
 
-B1 = tkinter.Button(f1, text='Go to frame 2', command=lambda:raise_frame(f2)).pack()
-B2 = tkinter.Button(f1, text='Quit', command=lambda:quit()).pack()
-Label(f1, text='FRAME 1').pack()
+#menu frame
+b1 = tkinter.Button(f1,text='Go to frame 2', command=lambda:raise_frame(f2)).grid(row=1, column=5, sticky=W)
+b2 = tkinter.Button(f1, text='Quit', command=lambda:quit()).grid(row=2, column=5, sticky=W)
+Label(f1, text='Menu').grid(row=3, column=5, sticky=W)
 
-Label(f2, text='FRAME 2').pack()
-B3 = tkinter.Button(f2, text='Go to frame 1', command=lambda:raise_frame(f1)).pack()
+#countries frame
+Label(f2, text='Map').grid(row=2, column=5, sticky=W)
+lbl = ttk.Label(f2, text = "Enter search query").grid(row=4, column=6, sticky=W)
+b3 = tkinter.Button(f2, text='Go to frame 1', command=lambda:raise_frame(f1)).grid(row=4, column=5, sticky=W)
+Label(f2, text='Search Countries Here!').grid(row=3, column=5, sticky=W)
 
 raise_frame(f1)
 root.mainloop()

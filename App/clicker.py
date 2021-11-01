51f619a61b3ae37bd54a5f2a8a229664e4b07377
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2021-11-01 09:57:52 +0000

    Update clicker.py

--- App/clicker.py
+++ App/clicker.py
@@ -19,20 +19,23 @@ s = Frame(root)
 for frame in (f1, f2, s):
     frame.grid(row=5, column=5, sticky='news')
 
+img = PhotoImage(file='Assets\images.png')
+
 #menu frame
 b1 = tkinter.Button(f1,text='Go to frame 2', command=lambda:raise_frame(f2)).grid(row=1, column=5, sticky=W)
 b2 = tkinter.Button(f1, text='Quit', command=lambda:quit()).grid(row=2, column=5, sticky=W)
-Label(f1, text='Menu').grid(row=3, column=5, sticky=W)
+ml = tkinter.Label(f1, text='Menu').grid(row=3, column=5, sticky=W)
 
 #game frame
-Label(f2, text='Game').grid(row=2, column=1, sticky=W)
+gl = tkinter.Label(f2, text='Game').grid(row=2, column=1, sticky=W)
 b3 = tkinter.Button(f2, text='Go to frame 1', command=lambda:raise_frame(f1)).grid(row=1, column=1, sticky=W)
 b4 = tkinter.Button(f2, text='Go to the shop', command=lambda :raise_frame(s)).grid(row=3, column=3, sticky=W)
+gc = tkinter.Label(f2,image=img).grid(row=3, column=3, sticky=W)
 
 
 #shop frame
-s1 = tkinter.Button(s,text='Return', command=lambda:raise_frame(f2)).grid(row=1, column=5, sticky=W)
-Label(f1, text='Menu').grid(row=3, column=5, sticky=W)
+st = tkinter.Label(s, text='The shop').grid(row=1, column=5, sticky=W)
+s1 = tkinter.Button(s,text='Return', command=lambda:raise_frame(f2)).grid(row=1, column=1, sticky=W)
 
 
 raise_frame(f1)

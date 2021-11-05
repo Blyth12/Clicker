3e889d36bdb18ced17aea7cc0070e895d4e8698b
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2021-11-05 13:27:57 +0000

    Update clicker.py

--- App/clicker.py
+++ App/clicker.py
@@ -10,33 +10,46 @@ def raise_frame(frame):
 
 root = Tk()
 
-root.geometry('500x500')
+root.geometry('300x200')
 
 f1 = Frame(root)
 f2 = Frame(root)
 s = Frame(root)
 
-for frame in (f1, f2, s):
-    frame.grid(row=5, column=5, sticky='news')
+root.resizable(width=False, height=False)
 
-img = PhotoImage(file='Assets\images.png')
+for frame in (f1, f2, s):
+    frame.grid(row=20, column=20, sticky='news')
+
+
+clicks = tkinter.IntVar()
+
+
+def clicked(event = None):
+   clicks.set(clicks.get() + 1)
+
+
+#img = PhotoImage(file='Assets\c.png')
 
 #menu frame
-b1 = tkinter.Button(f1,text='Go to frame 2', command=lambda:raise_frame(f2)).grid(row=1, column=5, sticky=W)
-b2 = tkinter.Button(f1, text='Quit', command=lambda:quit()).grid(row=2, column=5, sticky=W)
-ml = tkinter.Label(f1, text='Menu').grid(row=3, column=5, sticky=W)
+b1 = tkinter.Button(f1,text='Go to game', fg="dark green", bg="white", command=lambda:raise_frame(f2)).grid(row=1, column=2, sticky=W)
+b2 = tkinter.Button(f1, text='Quit', fg="red", bg="white", command=lambda:quit()).grid(row=1, column=3, sticky=W)
+ml = tkinter.Label(f1, text='Menu', fg="dark green", bg="white").grid(row=1, column=1, sticky=W)
 
 #game frame
-gl = tkinter.Label(f2, text='Game').grid(row=2, column=1, sticky=W)
-b3 = tkinter.Button(f2, text='Go to frame 1', command=lambda:raise_frame(f1)).grid(row=1, column=1, sticky=W)
-b4 = tkinter.Button(f2, text='Go to the shop', command=lambda :raise_frame(s)).grid(row=3, column=3, sticky=W)
-gc = tkinter.Label(f2,image=img).grid(row=3, column=3, sticky=W)
+gl = tkinter.Label(f2, text='Game', fg="dark green", bg="white").grid(row=1, column=1, sticky=W)
+b3 = tkinter.Button(f2, text='Return to menu', fg="dark green", bg="white", command=lambda:raise_frame(f1)).grid(row=1, column=2, sticky=W)
+b4 = tkinter.Button(f2, text='Go to the shop', fg="dark green", bg="white", command=lambda:raise_frame(s)).grid(row=1, column=3, sticky=W)
 
+#gc = tkinter.Label(f2,image=img).grid(row=3, column=3, sticky=W)
+bs = tkinter.Label(f2, text='').grid(row=5, column=3, sticky=W)
+bs1 = tkinter.Label(f2, text='').grid(row=6, column=3, sticky=W)
+tkinter.Button(f2, text="Increase", command=clicked, fg="dark green", bg="white").grid(row=7, column=3, sticky=W)
+ca = tkinter.Label(f2, textvariable=clicks, fg="dark green", bg="white").grid(row=8, column=3, sticky=W)
 
 #shop frame
-st = tkinter.Label(s, text='The shop').grid(row=1, column=5, sticky=W)
-s1 = tkinter.Button(s,text='Return', command=lambda:raise_frame(f2)).grid(row=1, column=1, sticky=W)
-
+st = tkinter.Label(s, text='The shop', fg="dark green", bg="white").grid(row=1, column=1, sticky=W)
+s1 = tkinter.Button(s,text='Return', fg="dark green", bg="white", command=lambda:raise_frame(f2)).grid(row=1, column=2, sticky=W)
 
 raise_frame(f1)
 root.mainloop()

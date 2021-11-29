d90b304d05a0857b2e7682b015a486acd9fbe5a5
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2021-11-29 09:57:37 +0000

    Update clicker.py

--- App/clicker.py
+++ App/clicker.py
@@ -1,26 +1,33 @@
-import tkinter
-from tkinter import ttk
+import tkinter as tk
 from tkinter import *
 import os
+
 
 def raise_frame(frame):
     frame.tkraise()
 
 
-root = Tk()
+root = tk.Tk()
+root.geometry('300x500')
+root.title('o')
 
-root.geometry('300x200')
-root.title('I hate tillmann')
+rootg = tk.Tk()
+rootg.geometry('300x500')
+rootg.title('o')
 
-f1 = Frame(root)
-f2 = Frame(root)
-s = Frame(root)
+roots = tk.Tk()
+roots.geometry('300x500')
+roots.title('o')
 
+
+f1 = tk.Frame(root)
+f2 = tk.Frame(rootg)
+s = tk.Frame(roots)
 
 root.resizable(width=False, height=False)
 
 for frame in (f1, f2, s):
-    frame.grid(row=20, column=20, sticky='news')
+    frame.grid(row=3000, column=2000, sticky='news')
 
 
 clicks = IntVar()
@@ -34,31 +41,31 @@ def increase(event=None):
 def clicked(event=None):
    clicks.set(clicks.get() + inc.get())
 
-
 #img = PhotoImage(file='Assets\c.png')
+#gc = tk.Label(f2,image=img).place(x=10, y=10)
 
 #menu frame
-b1 = tkinter.Button(f1,text='Go to game', fg="dark green", bg="white", command=lambda:raise_frame(f2)).grid(row=1, column=2, sticky=W)
-b2 = tkinter.Button(f1, text='Quit', fg="red", bg="white", command=lambda:quit()).grid(row=1, column=3, sticky=W)
-ml = tkinter.Label(f1, text='Menu', fg="dark green").grid(row=1, column=1, sticky=W)
+
+
+b1 = tk.Button(root, width=15, height=5, text='Go to game', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), root.withdraw()]).place(x=5, y=10)
+b2 = tk.Button(root, width=5, height=5, text='Quit', fg="red", bg="white", command=lambda:quit()).place(x=250, y=410)
+ml = tk.Label(root, width=5, height=5, text='Menu', fg="dark green").place(x=270, y=10, anchor='c')
 
 #game frame
-gl = tkinter.Label(f2, text='Game', fg="dark green").grid(row=1, column=1, sticky=W)
-b3 = tkinter.Button(f2, text='Return to menu', fg="dark green", bg="white", command=lambda:raise_frame(f1)).grid(row=1, column=2, sticky=W)
-b4 = tkinter.Button(f2, text='Go to the shop', fg="dark green", bg="white", command=lambda:raise_frame(s)).grid(row=1, column=3, sticky=W)
-#gc = tkinter.Label(f2,image=img).grid(row=3, column=3, sticky=W)
-bs = tkinter.Label(f2, text='').grid(row=5, column=3, sticky=W)
-bs1 = tkinter.Label(f2, text='').grid(row=6, column=3, sticky=W)
-ic = tkinter.Button(f2, text="Increase", command=clicked, fg="dark green", bg="white").grid(row=7, column=3, sticky=W)
-ca = tkinter.Label(f2, textvariable=clicks, fg="dark green").grid(row=8, column=3, sticky=W)
+gl = tk.Label(rootg, width=5, height=5, text='Game', fg="dark green").place(x=270, y=10, anchor='c')
+b3 = tk.Button(rootg, width=15, height=5,  text='Return to menu', fg="dark green", bg="white", command=lambda:[rootg.withdraw(), root.deiconify()]).place(x=175, y=410)
+b4 = tk.Button(rootg, width=15, height=5, text='Go to the shop', fg="dark green", bg="white", command=lambda:[roots.deiconify(), root.withdraw()]).place(x=10, y=410)
+ic = tk.Button(rootg, width=10, height=5, text="Increase", command=clicked, fg="dark green", bg="white").place(x=150, y=300, anchor='c')
+ca = tk.Label(rootg, width=5, height=5, text='c', textvariable=clicks, fg="dark green").place(x=150, y=100, anchor='c')
 
 #shop frame
-st = tkinter.Label(s, text='The shop', fg="dark green").grid(row=1, column=1, sticky=W)
-s1 = tkinter.Button(s, text='Return', fg="dark green", bg="white", command=lambda:raise_frame(f2)).grid(row=1, column=2, sticky=W)
-bs3 = tkinter.Label(s, text='').grid(row=2, column=1, sticky=W)
-bb1 = tkinter.Button(s, text="Test (+2 ppc)", command=increase, fg="dark green", bg="white").grid(row=3, column=1, sticky=W)
-ds = tkinter.Label(s, textvariable=clicks, fg="dark green").grid(row=3, column=2, sticky=W)
-mp = tkinter.Label(s, textvariable=inc, fg="dark green").grid(row=3, column=3, sticky=E)
+st = tk.Label(roots, width=5, height=5, text='The shop', fg="dark green").place(x=270, y=10, anchor='c')
+s1 = tk.Button(roots, width=5, height=5, text='Return', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), roots.withdraw()]).place(x=10, y=1)
+bb1 = tk.Button(roots, width=5, height=5, text="Test (+2 ppc)", command=increase, fg="dark green", bg="white").place(x=1, y=50)
+ds = tk.Label(roots, width=5, height=5, textvariable=clicks, fg="dark green").place(x=10, y=10)
+mp = tk.Label(roots, width=5, height=5, textvariable=inc, fg="dark green").place(x=50, y=1)
 
+roots.withdraw()
+rootg.withdraw()
 raise_frame(f1)
 root.mainloop()

0da407df4cacbb66a54c3b88429448678f0a78c2
Elliott Blyth Larrubia <86171441+Blyth12@users.noreply.github.com>
2021-12-16 14:50:19 +0000

    works

--- App/clicker.py
+++ App/clicker.py
@@ -7,24 +7,31 @@ def raise_frame(frame):
     frame.tkraise()
 
 
-root = tk.Tk()
+root = Tk()
 root.geometry('300x500')
-root.title('o')
+root.title('Menu')
 
-rootg = tk.Tk()
+rootg = Toplevel()
 rootg.geometry('300x500')
-rootg.title('o')
+rootg.title('Game')
 
-roots = tk.Tk()
+roots = Toplevel()
 roots.geometry('300x500')
-roots.title('o')
+roots.title('Shop')
+
+rootsu = Toplevel()
+rootsu.geometry('300x500')
+rootsu.title('Login')
 
 
 f1 = tk.Frame(root)
 f2 = tk.Frame(rootg)
 s = tk.Frame(roots)
+sl = tk.Frame(rootsu)
 
 root.resizable(width=False, height=False)
+rootg.resizable(width=False, height=False)
+roots.resizable(width=False, height=False)
 
 for frame in (f1, f2, s):
     frame.grid(row=3000, column=2000, sticky='news')
@@ -34,38 +41,49 @@ clicks = IntVar()
 inc = IntVar()
 
 
+def startinc(event=None):
+    inc.set(inc.get() + 1)
+
+
 def increase(event=None):
     inc.set(inc.get() + 1)
 
 
 def clicked(event=None):
-   clicks.set(clicks.get() + inc.get())
+    clicks.set(clicks.get() + inc.get())
+
 
 #img = PhotoImage(file='Assets\c.png')
 #gc = tk.Label(f2,image=img).place(x=10, y=10)
 
+
+#login frame
+tk.Button(rootsu, width=15, height=5, text='Login', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), root.withdraw()]).place(x=5, y=10)
+tk.Button(rootsu, width=5, height=5, text='Quit', fg="red", bg="white", command=lambda:quit()).place(x=250, y=410)
+tk.Label(rootsu, width=5, height=5, text='Menu', fg="dark green").place(x=270, y=10, anchor='c')
+
 #menu frame
-
-
-b1 = tk.Button(root, width=15, height=5, text='Go to game', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), root.withdraw()]).place(x=5, y=10)
-b2 = tk.Button(root, width=5, height=5, text='Quit', fg="red", bg="white", command=lambda:quit()).place(x=250, y=410)
-ml = tk.Label(root, width=5, height=5, text='Menu', fg="dark green").place(x=270, y=10, anchor='c')
+tk.Button(root, width=15, height=5, text='Signup/Login', fg="dark green", bg="white", command=lambda:[rootsu.deiconify(), root.withdraw()]).place(x=5, y=10)
+tk.Button(root, width=5, height=5, text='Quit', fg="red", bg="white", command=lambda:quit()).place(x=250, y=410)
+tk.Label(root, width=5, height=5, text='Menu', fg="dark green").place(x=270, y=10, anchor='c')
 
 #game frame
-gl = tk.Label(rootg, width=5, height=5, text='Game', fg="dark green").place(x=270, y=10, anchor='c')
-b3 = tk.Button(rootg, width=15, height=5,  text='Return to menu', fg="dark green", bg="white", command=lambda:[rootg.withdraw(), root.deiconify()]).place(x=175, y=410)
-b4 = tk.Button(rootg, width=15, height=5, text='Go to the shop', fg="dark green", bg="white", command=lambda:[roots.deiconify(), root.withdraw()]).place(x=10, y=410)
-ic = tk.Button(rootg, width=10, height=5, text="Increase", command=clicked, fg="dark green", bg="white").place(x=150, y=300, anchor='c')
-ca = tk.Label(rootg, width=5, height=5, text='c', textvariable=clicks, fg="dark green").place(x=150, y=100, anchor='c')
+tk.Label(rootg, width=5, height=5, text='Game', fg="dark green").place(x=270, y=10, anchor='c')
+tk.Button(rootg, width=15, height=5,  text='Return to menu', fg="dark green", bg="white", command=lambda:[rootg.withdraw(), root.deiconify()]).place(x=175, y=410)
+tk.Button(rootg, width=15, height=5, text='Go to the shop', fg="dark green", bg="white", command=lambda:[roots.deiconify(), root.withdraw()]).place(x=10, y=410)
+tk.Button(rootg, width=10, height=5, text="Increase", command=clicked, fg="dark green", bg="white").place(x=150, y=300, anchor='c')
+tk.Label(rootg, width=5, height=5, textvariable=clicks, fg="dark green").place(x=150, y=100, anchor='c')
 
 #shop frame
-st = tk.Label(roots, width=5, height=5, text='The shop', fg="dark green").place(x=270, y=10, anchor='c')
-s1 = tk.Button(roots, width=5, height=5, text='Return', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), roots.withdraw()]).place(x=10, y=1)
-bb1 = tk.Button(roots, width=5, height=5, text="Test (+2 ppc)", command=increase, fg="dark green", bg="white").place(x=1, y=50)
-ds = tk.Label(roots, width=5, height=5, textvariable=clicks, fg="dark green").place(x=10, y=10)
-mp = tk.Label(roots, width=5, height=5, textvariable=inc, fg="dark green").place(x=50, y=1)
+tk.Label(roots, width=6, height=5, text='The shop', fg="dark green").place(x=270, y=10, anchor='c')
+tk.Button(roots, width=15, height=5, text='Return', fg="dark green", bg="white", command=lambda:[rootg.deiconify(), roots.withdraw()]).place(x=175, y=410)
+tk.Button(roots, width=15, height=5, text="Test (+2 ppc)", command=increase, fg="dark green", bg="white").place(x=1, y=50)
+tk.Label(roots, width=5, height=5, textvariable=clicks, fg="dark green").place(x=175, y=150, anchor = 'c')
+tk.Label(roots, width=5, height=5, textvariable=inc, fg="dark green").place(x=175, y=100, anchor='c')
 
 roots.withdraw()
 rootg.withdraw()
+rootsu.withdraw()
+startinc()
 raise_frame(f1)
 root.mainloop()
